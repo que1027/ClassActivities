6fc01121b3c2767f76e6523837f98acc7b308908
Quinton Taylor <quintontaylor29@gmail.com>
2024-04-17 19:41:09 -0500

    JS cariables and primitives part 1 and 2

---
+++ Section02/2.2/JSVariablesAndPrimatives.js
@@ -0,0 +1,34 @@
+// create a constant variable (const)
+const firstName = "Quinton";
+// create a variable that can be reassigned (let)
+let age = 24;
+// create three variables and assign them values of different data types (=)
+let favoriteColor = "Green";
+let favoriteNumber = 7;
+const isDrinkingAge = true;
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof favoriteColor);
+console.log(typeof isDrinkingAge);
+// change the value referenced by a variable (dynamic typing)
+favoriteNumber = "7";
+// print the type of the variable you just changed (typeof)
+console.log(typeof favoriteNumber);
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+let language = "Javascript";
+let tme = 1;
+let isMaster = false;
+// What type of data needs to go in each variable?
+
+console.log(
+  "Hello, my name is " +
+    firstName +
+    ", and I am learning " +
+    language +
+    ". I have been practicing for only " +
+    tme +
+    " weeks, so my status as a master is: " +
+    isMaster +
+    "."
+);
