421ccef63e09ccd4056f995f2f4afe100d952008
Quinton Taylor <quintontaylor29@gmail.com>
2024-04-22 21:34:18 -0500

    week3 monday class acticities

---
+++ Section03/3.2/3.1.4-Parameters.js
@@ -0,0 +1,24 @@
+// Named Parameters
+// write a function that takes two named parameters:
+// print each named parameter,
+// then return the parameters added together
+function functionWithTwoParams(x, y) {
+  console.log("x = ", x);
+  console.log("y = ", y);
+  return x + y;
+}
+// invoke the function and pass in two numbers
+functionWithTwoParams(3, 4);
+// invoke the function and pass in more than two numbers
+functionWithTwoParams(3, 4, 5, 6);
+// invoke the function and pass in only one number
+functionWithTwoParams(3);
+// change the function to set default values for the parameters
+
+// again, invoke the function and pass in only one number
+
+// Rest Operator
+// add a rest operator to the function's parameters
+// add a line to the function's body to print the value of the rest operator
+
+// again, invoke the function and pass in more than two numbers
